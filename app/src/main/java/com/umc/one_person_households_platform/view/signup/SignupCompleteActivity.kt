5b9e5dc361df708063afdb5402ef88d8c6089753
Jayla-Y <jay00617@gmail.com>
2023-08-04 15:37:19 +0900

    feature: 로그인,회원가입 UI 구현

---
+++ app/src/main/java/com/umc/one_person_households_platform/view/signup/SignupCompleteActivity.kt
@@ -0,0 +1,12 @@
+package com.umc.one_person_households_platform.view.signup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.umc.one_person_households_platform.R
+
+class SignupCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_complete)
+    }
+}
